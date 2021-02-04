0f56f98590745a0d3eaa0723943b90ff2006493f
byplayer <byplayer100@gmail.com>
2021-02-04 15:05:44 +0900

    :tada: initial commit
    add chrono-node test code

---
+++ bin/index.js
@@ -0,0 +1,5 @@
+const chrono = require("chrono-node");
+
+console.log(chrono.parseDate("An appointment on Sep 12-13"));
+console.log(chrono.parseDate("today"));
+console.log(chrono.parseDate("next Monday"));
